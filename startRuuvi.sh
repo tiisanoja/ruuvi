8bdc946e65b250eed9e80ce85c43b314767224b0
Jari Tiisanoja <107865689+tiisanoja@users.noreply.github.com>
2022-07-05 10:54:24 +0300

    Fixed some typos

--- startRuuvi.sh
+++ startRuuvi.sh
@@ -4,13 +4,13 @@ sleep 15
 
 hciattach /dev/ttyAMA0 bcm43xx 921600
 
-#Set green led on to indicate that we are receiving measurements
+#Set green led on to indicate we are receiving measurements
 #You can comment this out if you are not using RaspberryPi
 echo 1 > /sys/class/leds/ACT/brightness
 
 ./ruuvi >/dev/null 2>&1
 
-#Trun off green led to indicate that we are not running application anymore
+#Turn off green led to indicate that we are not running application anymore
 #This will help to see if ruuvi application is not running anymore
-#You can comment nexy row  if you are not using RaspberryPi
+#You can comment next row if you are not using RaspberryPi
 echo 0 > /sys/class/leds/ACT/brightness
