9d9669e37d439e047d1185a69fae122b78850d16
Jari Tiisanoja <tiisanoja@gmail.com>
2022-07-06 06:53:05 +0300

    Logs are stored to file under /var/log/ruuvi.

--- startRuuvi.sh
+++ startRuuvi.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+#Create directory for the logs if it does not exists
+mkdir -p /var/log/ruuvi
+
 sleep 15
 
 hciattach /dev/ttyAMA0 bcm43xx 921600
 
-#Set green led on to indicate that we are receiving measurements
-#You can comment this out if you are not using RaspberryPi
-echo 1 > /sys/class/leds/ACT/brightness
+/usr/local/bin/LED-act-on.sh
 
-./ruuvi >/dev/null 2>&1
+#./ruuvi >/dev/null 2>&1
+./ruuvi 2>&1 |grep -va 'Ruuvi data with'|grep -va ' DATA: \[ '|grep -va 'INFO: Skipping'|grep -va ' MAC: ' >> /var/log/ruuvi/ruuvi.`date +%Y%m%d`.log 
 
-#Trun off green led to indicate that we are not running application anymore
-#This will help to see if ruuvi application is not running anymore
-#You can comment nexy row  if you are not using RaspberryPi
-echo 0 > /sys/class/leds/ACT/brightness
+/usr/local/bin/LED-act-off.sh
