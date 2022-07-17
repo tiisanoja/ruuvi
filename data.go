e93bb9a1beca52b8b78bfdd14ee2e066a0cd68be
Jari Tiisanoja <jari.tiisanoja@tietoevry.com>
2022-07-18 00:52:04 +0300

    InfluxDB2 driver changes

--- data.go
+++ data.go
@@ -1,173 +1,167 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
-	"os"
-	"time"
-	"log"
-	"math"
-	"sync"
+    "bytes"
+    "encoding/binary"
+    "fmt"
+    "log"
+    "math"
+    "os"
+    "sync"
+    "time"
 )
 
 var (
-	httpURL = os.Getenv("HTTP_URL")
+    httpURL = os.Getenv("HTTP_URL")
 )
 
 //SensorData to be posted
 type SensorData struct {
-	Temp          float64
-	Humidity      float64
-	Pressure      uint32
-	Battery       uint16
-	Address       string
-	AccelerationX int16
-	AccelerationY int16
-	AccelerationZ int16
-	TimeStamp     time.Time
+    Temp          float64
+    Humidity      float64
+    Pressure      uint32
+    Battery       uint16
+    Address       string
+    AccelerationX int16
+    AccelerationY int16
+    AccelerationZ int16
+    TimeStamp     time.Time
     Seq           uint16
-	TXPower       int16
+    TXPower       int16
     MAC           string
 }
 
 type SensorFormat5 struct {
-        ManufacturerID      uint16
-        DataFormat          uint8
-        Temperature         int16
-        Humidity            uint16
-        Pressure            uint16
-        AccelerationX       int16
-        AccelerationY       int16
-        AccelerationZ       int16
-        BatteryVoltageMv    uint16
-        Movement            uint8
-        Sequence            uint16
-        MAC1                uint8
-        MAC2                uint8
-        MAC3                uint8
-        MAC4                uint8
-        MAC5                uint8
-        MAC6                uint8
+    ManufacturerID   uint16
+    DataFormat       uint8
+    Temperature      int16
+    Humidity         uint16
+    Pressure         uint16
+    AccelerationX    int16
+    AccelerationY    int16
+    AccelerationZ    int16
+    BatteryVoltageMv uint16
+    Movement         uint8
+    Sequence         uint16
+    MAC1             uint8
+    MAC2             uint8
+    MAC3             uint8
+    MAC4             uint8
+    MAC5             uint8
+    MAC6             uint8
 }
-
-
 
 func parseTemperature(t uint8, f uint8) float64 {
-	var mask uint8
-	mask = (1 << 7)
-	isNegative := (t & mask) > 0
-	temp := float64(t&^mask) + float64(f)/100.0
-	if isNegative {
-		temp *= -1
-	}
-	return temp
+    var mask uint8
+    mask = (1 << 7)
+    isNegative := (t & mask) > 0
+    temp := float64(t&^mask) + float64(f)/100.0
+    if isNegative {
+        temp *= -1
+    }
+    return temp
 }
-
 
 var mWait map[string]bool = make(map[string]bool)
 var mutex = &sync.Mutex{}
 
 func parseSensorFormat5(data []byte) *SensorData {
-        reader := bytes.NewReader(data)
-        result := SensorFormat5{}
-        sensorData := SensorData{}
+    reader := bytes.NewReader(data)
+    result := SensorFormat5{}
+    sensorData := SensorData{}
 
-        err := binary.Read(reader, binary.BigEndian, &result)
-        if err != nil {
-                log.Printf("ERROR: %s\n",err)
-                return &sensorData
-        }
+    err := binary.Read(reader, binary.BigEndian, &result)
+    if err != nil {
+        log.Printf("ERROR: %s\n", err)
+        return &sensorData
+    }
 
-        sensorData.Temp = float64(result.Temperature)*0.005
-        sensorData.Humidity = float64(result.Humidity)* 0.0025 
-        sensorData.Pressure = uint32(result.Pressure) + uint32(50000 + PressureCorrection)
-        bat := result.BatteryVoltageMv & 0xffe0
-        bat = ((bat >> 5) + 1600) 
-        sensorData.Battery = bat
-        sensorData.TXPower = int16(0x001F & result.BatteryVoltageMv )* 2 - 40
-        sensorData.AccelerationX = result.AccelerationX
-        sensorData.AccelerationY = result.AccelerationY
-        sensorData.AccelerationZ = result.AccelerationZ
-        sensorData.Seq = result.Sequence
-        sensorData.MAC = fmt.Sprintf("%x:%x:%x:%x:%x:%x",result.MAC1, result.MAC2, result.MAC3, result.MAC4, result.MAC5, result.MAC6)
+    sensorData.Temp = float64(result.Temperature) * 0.005
+    sensorData.Humidity = float64(result.Humidity) * 0.0025
+    sensorData.Pressure = uint32(result.Pressure) + uint32(50000+PressureCorrection)
+    bat := result.BatteryVoltageMv & 0xffe0
+    bat = ((bat >> 5) + 1600)
+    sensorData.Battery = bat
+    sensorData.TXPower = int16(0x001F&result.BatteryVoltageMv)*2 - 40
+    sensorData.AccelerationX = result.AccelerationX
+    sensorData.AccelerationY = result.AccelerationY
+    sensorData.AccelerationZ = result.AccelerationZ
+    sensorData.Seq = result.Sequence
+    sensorData.MAC = fmt.Sprintf("%x:%x:%x:%x:%x:%x", result.MAC1, result.MAC2, result.MAC3, result.MAC4, result.MAC5, result.MAC6)
 
-        mutex.Lock()
-        if mWait[sensorData.MAC] == true {
-            mutex.Unlock()
-            log.Printf("INFO: Skipping %s\n",sensorData.MAC)
-            return &sensorData
-        }
-        mWait[sensorData.MAC] = true
+    mutex.Lock()
+    if mWait[sensorData.MAC] == true {
         mutex.Unlock()
+        log.Printf("INFO: Skipping %s\n", sensorData.MAC)
+        return &sensorData
+    }
+    mWait[sensorData.MAC] = true
+    mutex.Unlock()
 
-	fmt.Printf("Seq: %d, Temp: %f, Pres: %d, Hum: %f, Battery: %d, TXPower: %d, MAC: %s\n",
-          sensorData.Seq, sensorData.Temp, sensorData.Pressure, sensorData.Humidity, sensorData.Battery, sensorData.TXPower, sensorData.MAC);
+    fmt.Printf("Seq: %d, Temp: %f, Pres: %d, Hum: %f, Battery: %d, TXPower: %d, MAC: %s\n",
+        sensorData.Seq, sensorData.Temp, sensorData.Pressure, sensorData.Humidity, sensorData.Battery, sensorData.TXPower, sensorData.MAC)
 
-        measurement := make(map[string]interface{})
-        measurement["Temperature"] = sensorData.Temp
-        if sensorData.Humidity < 150.0 { 
-             measurement["Humidity"] = sensorData.Humidity
-        }
+    measurement := make(map[string]interface{})
+    measurement["Temperature"] = sensorData.Temp
+    if sensorData.Humidity < 150.0 {
+        measurement["Humidity"] = sensorData.Humidity
+    }
 
-        steamSaturationPressure := 6.1078 * math.Pow(10, (7.5*sensorData.Temp / (sensorData.Temp+237.3)))
-        absHumidity := (216.679*(sensorData.Humidity * steamSaturationPressure)/100) / (sensorData.Temp+273.15)
-        measurement["AbsoluteHumidity"] = absHumidity
+    steamSaturationPressure := 6.1078 * math.Pow(10, (7.5*sensorData.Temp/(sensorData.Temp+237.3)))
+    absHumidity := (216.679 * (sensorData.Humidity * steamSaturationPressure) / 100) / (sensorData.Temp + 273.15)
+    measurement["AbsoluteHumidity"] = absHumidity
 
-        //Dew Point
-        tTemp:=((17.27*sensorData.Temp)/(237.7+sensorData.Temp))+math.Log(sensorData.Humidity/100)
-        tDewpoint := 0.0
-        if(tTemp != 17.27){ 
-                tDewpoint =(237.7*tTemp)/(17.27-tTemp)
-        } else{
-		fmt.Printf("INFO: In tDewpoint: Dividing by zero. Adding 0.01 to temperature.\n")
-                tDewpoint =(237.7*(tTemp+0.01))/(17.27-tTemp+0.01)
-        }
+    //Dew Point
+    tTemp := ((17.27 * sensorData.Temp) / (237.7 + sensorData.Temp)) + math.Log(sensorData.Humidity/100)
+    tDewpoint := 0.0
+    if tTemp != 17.27 {
+        tDewpoint = (237.7 * tTemp) / (17.27 - tTemp)
+    } else {
+        fmt.Printf("INFO: In tDewpoint: Dividing by zero. Adding 0.01 to temperature.\n")
+        tDewpoint = (237.7 * (tTemp + 0.01)) / (17.27 - tTemp + 0.01)
+    }
 
-        measurement["Dewpoint"] = tDewpoint
-	measurement["Pressure"] = sensorData.Pressure
+    measurement["Dewpoint"] = tDewpoint
+    measurement["Pressure"] = sensorData.Pressure
 
-        Insert(measurement, sensorData.MAC)
+    Insert(measurement, sensorData.MAC)
 
-        HWmeasurement := make(map[string]interface{})
-        HWmeasurement["Battery"] = sensorData.Battery
-        HWmeasurement["TxPower"] = sensorData.TXPower
+    HWmeasurement := make(map[string]interface{})
+    HWmeasurement["Battery"] = sensorData.Battery
+    HWmeasurement["TxPower"] = sensorData.TXPower
 
-        InsertHW(HWmeasurement, sensorData.MAC)
+    InsertHW(HWmeasurement, sensorData.MAC)
 
     //Let's take only every 15s measurement per device
-        time.Sleep(StoreDelay )
-        mutex.Lock()
-        mWait[sensorData.MAC] = false
-	mutex.Unlock()
+    time.Sleep(StoreDelay)
+    mutex.Lock()
+    mWait[sensorData.MAC] = false
+    mutex.Unlock()
 
     return &sensorData
 }
 
-
 //ParseRuuviData parses ruuvidata
 func ParseRuuviData(data []byte, a string) {
 
-	if len(data) > 2 && binary.LittleEndian.Uint16(data[0:2]) == 0x0499 {	
-		if len(data) > 25 { 
-			sensorFormat := data[2]
-			fmt.Printf("Ruuvi data with sensor format %d\n", sensorFormat)
-			switch sensorFormat {
-				case 3:
-					fmt.Printf("RuuviTag version 3 not supported. Please upgrade RuuviTag to version 5.")
-				case 5:
-					parseSensorFormat5(data)
-				default:
-					fmt.Printf("Unknown sensor format %d\n", sensorFormat)
-			}
-		} else{
-			fmt.Printf("Incorrect lenght in Ruuvi device data: %d. Expected to be 26.\n",len(data))
-		} 	
-		
+    if len(data) > 2 && binary.LittleEndian.Uint16(data[0:2]) == 0x0499 {
+        if len(data) > 25 {
+            sensorFormat := data[2]
+            fmt.Printf("Ruuvi data with sensor format %d\n", sensorFormat)
+            switch sensorFormat {
+            case 3:
+                fmt.Printf("RuuviTag version 3 not supported. Please upgrade RuuviTag to version 5.")
+            case 5:
+                parseSensorFormat5(data)
+            default:
+                fmt.Printf("Unknown sensor format %d\n", sensorFormat)
+            }
+        } else {
+            fmt.Printf("Incorrect lenght in Ruuvi device data: %d. Expected to be 26.\n", len(data))
+        }
 
-
-	} else {
-		//fmt.Printf("Not a ruuvi device \n")
-	}
+    } else {
+        //fmt.Printf("Not a ruuvi device \n")
+    }
 
 }
--- db.go
+++ db.go
@@ -1,154 +1,102 @@
 package main
 
-
 import (
-	"log"
-	"time"
-        "github.com/influxdata/influxdb1-client/v2"
+    "log"
+    "time"
+
+    influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
 
 const (
-	// MyDB specifies name of database
-        MyDB = "weather"
-        DBERROR = "DB Error: "
+    // Specifies name of bucket where data is stored
+    MyBucket = "weather"
+    DBERROR  = "DB Error: "
 )
 
 // Insert points to database
 // Uses: Measurement table
 func Insert(measurement map[string]interface{}, MAC string) {
-	c, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr: ConnectionString,
-	})
-	if err != nil {
-	        log.Printf(DBERROR)
-		log.Println(err)
-                return
-	}
-	defer c.Close()
+    // Create client and set batch size to 2
+    c := influxdb2.NewClientWithOptions(ConnectionString, DBToken, influxdb2.DefaultOptions().SetBatchSize(2))
+    defer c.Close()
 
-	// Create a new point batch
-	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:  MyDB,
-		Precision: "s",
-	})
-	if err != nil {
-	        log.Printf(DBERROR)
-                log.Println(err)
-                return
-	}
+    // user blocking write client for writes to desired bucket
+    writeAPI := c.WriteAPI(DBOrg, MyBucket)
 
-        //
-        tags := map[string]string{"Address": Address}
+    // Get errors channel
+    errorsCh := writeAPI.Errors()
+    // Create go proc for reading and logging errors
+    go func() {
+        for err := range errorsCh {
+            log.Printf("DB Error: %s\n", err.Error())
+        }
+    }()
 
-	bFound := false
-	// Create a point and add to batch
-	for i := 0; i != len(aSensors); i++ {
-	    if MAC == aSensors[i] {
-	       tags["Location"] = aLocations[i]
-	       bFound = true
-	    }
-	}
+    //
+    tags := map[string]string{"Address": Address}
 
-	/*Check if MAC was found from the list to be stored to DB*/
-	if bFound == false {
-	   /*Not found. not storing*/
-	   log.Printf("%s Ruuvitag is not stored to DB.\n", MAC)
-	   return
-	}
+    bFound := false
+    // Create a point and add to batch
+    for i := 0; i != len(aSensors); i++ {
+        if MAC == aSensors[i] {
+            tags["Location"] = aLocations[i]
+            bFound = true
+        }
+    }
 
-	tags["Device"] = MAC
-	fields := measurement
+    /*Check if MAC was found from the list to be stored to DB*/
+    if bFound == false {
+        /*Not found. not storing*/
+        log.Printf("%s Ruuvitag is not stored to DB.\n", MAC)
+        return
+    }
 
-	pt, err := client.NewPoint("measurements", tags, fields, time.Now())
-	if err != nil {
-	        log.Printf(DBERROR)
-                log.Println(err)
-                return
-	}
-	bp.AddPoint(pt)
+    tags["Device"] = MAC
+    fields := measurement
 
-	// Write the batch
-	if err := c.Write(bp); err != nil {
-	        log.Printf(DBERROR)
-                log.Println(err)
-                return
-	}
+    pt := influxdb2.NewPoint("measurements", tags, fields, time.Now())
+    writeAPI.WritePoint(pt)
 
-   	// Close client resources
-	if err := c.Close(); err != nil {
-	        log.Printf(DBERROR)
-                log.Println(err)
-                return
-	}
+    // Force all unwritten data to be sent
+    writeAPI.Flush()
 }
 
 // Insert points to database
 //Uses Hardware table
 func InsertHW(measurement map[string]interface{}, MAC string) {
-        c, err := client.NewHTTPClient(client.HTTPConfig{
-                Addr: ConnectionString,
-        })
-        if err != nil {
-	        log.Printf(DBERROR)
-                log.Println(err)
-                return
+    // Create client and set batch size to 2
+    c := influxdb2.NewClientWithOptions(ConnectionString, "my-token", influxdb2.DefaultOptions().SetBatchSize(2))
+    defer c.Close()
+
+    // user blocking write client for writes to desired bucket
+    writeAPI := c.WriteAPI("my-org", MyBucket)
+
+    // Create a point and add to batch
+    tags := map[string]string{"Address": Address}
+
+    bFound := false
+    // Create a point and add to batch
+    for i := 0; i != len(aSensors); i++ {
+        if MAC == aSensors[i] {
+            tags["Location"] = aLocations[i]
+            bFound = true
         }
-        defer c.Close()
+    }
 
-        // Create a new point batch
-        bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-                Database:  MyDB,
-                Precision: "s",
-        })
-        if err != nil {
-	        log.Printf(DBERROR)
-                log.Println(err)
-                return
-        }
+    /*Check if MAC was found from the list to be stored to DB*/
+    if bFound == false {
+        /*Not found, not storing*/
+        log.Printf("%s Ruuvitag is not stored to DB.\n", MAC)
+        return
+    }
 
-        // Create a point and add to batch
-        tags := map[string]string{"Address": Address}
+    tags["Device"] = MAC
+    fields := measurement
 
-	bFound := false
-	// Create a point and add to batch
-	for i := 0; i != len(aSensors); i++ {
-	    if MAC == aSensors[i] {
-	       tags["Location"] = aLocations[i]
-	       bFound = true
-	    }
-	}
+    pt := influxdb2.NewPoint("HWmeasurements", tags, fields, time.Now())
+    writeAPI.WritePoint(pt)
 
-	/*Check if MAC was found from the list to be stored to DB*/
-	if bFound == false {
-	   /*Not found, not storing*/
-	   log.Printf("%s Ruuvitag is not stored to DB.\n", MAC)
-	   return
-	}
+    // Force all unwritten data to be sent
+    writeAPI.Flush()
 
-
-	tags["Device"] = MAC
-	
-        fields := measurement
-
-        pt, err := client.NewPoint("HWmeasurements", tags, fields, time.Now())
-        if err != nil {
-	        log.Printf(DBERROR)
-                log.Println(err)
-                return
-        }
-        bp.AddPoint(pt)
-
-        // Write the batch
-        if err := c.Write(bp); err != nil {
-	        log.Printf(DBERROR)
-                log.Println(err)
-                return
-        }
-
-        // Close client resources
-        if err := c.Close(); err != nil {
-	        log.Printf(DBERROR)
-                log.Println(err)
-                return
-        }
 }
